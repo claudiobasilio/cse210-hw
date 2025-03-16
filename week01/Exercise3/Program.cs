d6f03d3438559d1e77f759d18067fb3905619300
claudiobasilio <bas23006@byui.edu>
2025-03-16 17:15:50 -0600

    Finished assignment 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        /*Console.Write("What is the magic number? ");
+        String answer = Console.ReadLine();
+        int magicNumber = int.Parse(answer);
+
+        Console.Write("What is your guess? ");
+        string response = Console.ReadLine();
+        int guess = int.Parse(response);
+        */
+
+        int guess = -1; 
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
